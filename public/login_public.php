8fb960d687bce1b00baca8a3c0b5953c00b8ab2b
changchanghun <ch4856lee@naver.com>
2025-09-16 14:10:58 +0900

    수정

--- public/login_public.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include_once('back/public/dbConn.php');
-include_once('back/public/common.php');
-include_once('back/public/login_check.php');
-?>
-<!DOCTYPE html>
-<html lang="ko">
--- public/unLogin_public.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if($_SESSION['user_id']){
-        header('Location:http://chworld2.dothome.co.kr/agency/main.php');
-    }
-?>
-<!DOCTYPE html>
-<html lang="ko">
